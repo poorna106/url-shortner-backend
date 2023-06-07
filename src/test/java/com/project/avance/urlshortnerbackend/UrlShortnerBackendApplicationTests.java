3628bd0d3918ed74fe8728d77695a532c507c799
POORNA PANDA <poorna.panda@gmail.com>
2023-06-07 11:48:09 +0530

    Project structure

---
+++ src/test/java/com/project/avance/urlshortnerbackend/UrlShortnerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.avance.urlshortnerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortnerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
